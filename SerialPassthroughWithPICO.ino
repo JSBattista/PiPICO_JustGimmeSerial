5f891435ada65f299c0b2ed65a2f3d482af7294b
John S Battista <30750774+JSBattista@users.noreply.github.com>
2022-07-05 21:58:00 -0400

    Add files via upload

---
+++ SerialPassthroughWithPICO.ino
@@ -0,0 +1,22 @@
+/*
+ Just gimme serial!!!!!
+ Based on
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/SerialPassthrough
+*/
+
+void setup() {
+  Serial.begin(9600);   // Serial alone does not have set RX, TX
+  Serial1.setTX(0);
+  Serial1.setRX(1);
+  Serial1.begin(9600);
+  Serial2.setTX(4);
+  Serial2.setRX(5);
+  Serial2.begin(9600);
+}
+
+void loop() {
+  delay(1000);
+  Serial.write("Brown Cow\n");
+  Serial1.write("Hey\n");
+  Serial2.write("now\n");
+}
